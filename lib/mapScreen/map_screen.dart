64e19dcde3257389de99c7d1289e9c6b519bd632
nourah-saad <74484505+nourah-saad@users.noreply.github.com>
2022-02-23 11:55:15 +0300

    undo

--- lib/mapScreen/map_screen.dart
+++ lib/mapScreen/map_screen.dart
@@ -7,10 +7,6 @@ import 'package:provider/provider.dart';
 import 'package:tayseer2/assistants/assistant_methods.dart';
 import 'package:tayseer2/infoHandler/app_info.dart';
 import 'package:provider/provider.dart';
-import 'package:flutter_geofire/flutter_geofire.dart';
-import 'package:tayseer2/models/active_nearby_available_drivers.dart';
-
-import '../assistants/geofire_assistant.dart';
 
 class MapScreen extends StatefulWidget {
   @override
@@ -34,19 +30,6 @@ class _MapScreenState extends State<MapScreen> {
   LocationPermission? _locationPermission;
   double bottomPaddingOfMap = 0;
 
-<<<<<<< HEAD
-  String statusText = "Now Offline";
-  Color buttonColor = Colors.grey;
-  bool isDriverActive = false;
-
-  Set<Marker> markersSet = {};
-  Set<Circle> circlesSet = {};
-
-  bool activeNearbyDriverKeysLoaded = false;
-  BitmapDescriptor? activeNearbyIcon;
-
-=======
->>>>>>> parent of 686c067 (online - offline drivers)
   checkIfLocationPermissionAllowed() async {
     _locationPermission = await Geolocator.requestPermission();
 
@@ -75,174 +58,14 @@ class _MapScreenState extends State<MapScreen> {
     print("this is your address = " + humanReadableAddress);
   }
 
-  
-
   @override
   void initState() {
     super.initState();
     checkIfLocationPermissionAllowed();
   }
 
-<<<<<<< HEAD
-  driverIsOnlineNow() async {
-    Position pos = await Geolocator.getCurrentPosition(
-      desiredAccuracy: LocationAccuracy.high,
-    );
-    driverCurrentPosition = pos;
-
-    Geofire.initialize("activeDrivers");
-
-    Geofire.setLocation(currentFirebaseUser!.uid,
-        driverCurrentPosition!.latitude, driverCurrentPosition!.longitude);
-
-    DatabaseReference ref = FirebaseDatabase.instance
-        .ref()
-        .child("drivers")
-        .child(currentFirebaseUser!.uid)
-        .child("newRideStatus");
-
-    ref.set("idle"); //searching for ride request
-    ref.onValue.listen((event) {});
-  }
-
-  updateDriversLocationAtRealTime() {
-    streamSubscriptionPosition =
-        Geolocator.getPositionStream().listen((Position position) {
-      driverCurrentPosition = position;
-
-      if (isDriverActive == true) {
-        Geofire.setLocation(currentFirebaseUser!.uid,
-            driverCurrentPosition!.latitude, driverCurrentPosition!.longitude);
-      }
-
-      LatLng latLng = LatLng(
-        driverCurrentPosition!.latitude,
-        driverCurrentPosition!.longitude,
-      );
-
-      newGoogleMapController!.animateCamera(CameraUpdate.newLatLng(latLng));
-    });
-  }
-
-  driverIsOfflineNow() {
-    Geofire.removeLocation(currentFirebaseUser!.uid);
-
-    DatabaseReference? ref = FirebaseDatabase.instance
-        .ref()
-        .child("drivers")
-        .child(currentFirebaseUser!.uid)
-        .child("newRideStatus");
-    ref.onDisconnect();
-    ref.remove();
-    ref = null;
-
-    Future.delayed(const Duration(milliseconds: 2000), () {
-      SystemNavigator.pop();
-    });
-  }
-
-  initializeGeoFireListener() {
-    Geofire.initialize("activeDrivers");
-
-    Geofire.queryAtLocation(driverCurrentPosition!.latitude,
-            driverCurrentPosition!.longitude, 10)! // 10 Km around the car
-        .listen((map) {
-      print(map);
-      if (map != null) {
-        var callBack = map['callBack'];
-
-        //latitude will be retrieved from map['latitude']
-        //longitude will be retrieved from map['longitude']
-
-        switch (callBack) {
-          //whenever any driver become online
-          case Geofire.onKeyEntered:
-            ActiveNearbyAvailableDrivers activeNearbyAvailableDriver =
-                ActiveNearbyAvailableDrivers();
-            activeNearbyAvailableDriver.locationLatitude = map['latitude'];
-            activeNearbyAvailableDriver.locationLongitude = map['longitude'];
-            activeNearbyAvailableDriver.driverId = map['key'];
-            GeoFireAssistant.activeNearbyAvailableDriversList
-                .add(activeNearbyAvailableDriver);
-            if (activeNearbyDriverKeysLoaded == true) {
-              displayActiveDriversOnUsersMap();
-            }
-            break;
-
-          //whenever any driver become offline
-          case Geofire.onKeyExited:
-            GeoFireAssistant.deleteOfflineDriverFromList(map['key']);
-            displayActiveDriversOnUsersMap();
-            break;
-
-          //whenever driver moves - update driver location
-          case Geofire.onKeyMoved:
-            ActiveNearbyAvailableDrivers activeNearbyAvailableDriver =
-                ActiveNearbyAvailableDrivers();
-            activeNearbyAvailableDriver.locationLatitude = map['latitude'];
-            activeNearbyAvailableDriver.locationLongitude = map['longitude'];
-            activeNearbyAvailableDriver.driverId = map['key'];
-            GeoFireAssistant.updateActiveNearbyAvailableDriverLocation(
-                activeNearbyAvailableDriver);
-            displayActiveDriversOnUsersMap();
-            break;
-
-          //display those online drivers on user's map
-          case Geofire.onGeoQueryReady:
-            activeNearbyDriverKeysLoaded = true;
-            displayActiveDriversOnUsersMap();
-            break;
-        }
-      }
-
-      setState(() {});
-    });
-  }
-
-  displayActiveDriversOnUsersMap() {
-    setState(() {
-      markersSet.clear();
-      circlesSet.clear();
-
-      Set<Marker> driversMarkerSet = Set<Marker>();
-
-      for (ActiveNearbyAvailableDrivers eachDriver
-          in GeoFireAssistant.activeNearbyAvailableDriversList) {
-        LatLng eachDriverActivePosition =
-            LatLng(eachDriver.locationLatitude!, eachDriver.locationLongitude!);
-
-        Marker marker = Marker(
-          markerId: MarkerId("driver" + eachDriver.driverId!),
-          position: eachDriverActivePosition,
-          icon: activeNearbyIcon!,
-          rotation: 360,
-        );
-
-        driversMarkerSet.add(marker);
-      }
-
-      setState(() {
-        markersSet = driversMarkerSet;
-      });
-    });
-  }
-
-  createActiveNearByDriverIconMarker() {
-    if (activeNearbyIcon == null) {
-      ImageConfiguration imageConfiguration =
-          createLocalImageConfiguration(context, size: const Size(2, 2));
-      BitmapDescriptor.fromAssetImage(imageConfiguration, "images/car.png")
-          .then((value) {
-        activeNearbyIcon = value;
-      });
-    }
-  }
-
-=======
->>>>>>> parent of 686c067 (online - offline drivers)
   @override
   Widget build(BuildContext context) {
-    createActiveNearByDriverIconMarker();
     return Scaffold(
       appBar: AppBar(),
       body: Stack(
